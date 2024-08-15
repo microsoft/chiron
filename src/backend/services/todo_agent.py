1548758815c2ecdbc8f3c61569f1b719f50e7282
MJ Schanne <mjschanne@microsoft.com>
2024-08-15 13:39:59 -0400

    Resolve serialization issues in todo_agent

--- src/backend/services/todo_agent.py
+++ src/backend/services/todo_agent.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from helper.azure_config import AzureConfig
+import json
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.prompts.chat import (
-    ChatPromptTemplate,
-    MessagesPlaceholder
-)
+from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import tool
 from langchain_openai import AzureChatOpenAI
+from typing import List
 import uuid
+
 
 class Todo:
     def __init__(self, task: str, due_date: datetime, tags: list[str] = []):
@@ -18,25 +18,49 @@ class Todo:
         self.completed = False
         self.tags = tags
 
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__ if not isinstance(o, datetime) else o.isoformat())
+
+
 config = AzureConfig()
 
 llm = AzureChatOpenAI(
     deployment_name=config.azure_deployment,
-    openai_api_version=config.azure_openai_api_version
-)  
-todos = []
+    openai_api_version=config.azure_openai_api_version,
+)
+todos: List[Todo] = [
+    Todo("Take the cat to the vet", datetime(2024, 8, 31)),
+    Todo("Take son to dentist", datetime(2024, 8, 16)),
+    Todo("Get groceries", datetime(2024, 8, 17)),
+    Todo("Get my head checked", datetime(1980, 12, 25)),
+]
+
 
 ### Tools ### considering moving to separate directory
 @tool
-def create_todo(todo: str, datetime: datetime) -> Todo: # add optional tags and due date for end of week
+def create_todo(
+    todo: str, datetime: datetime
+) -> Todo:  # add optional tags and due date for end of week
     """Create a new todo."""
-    todos.append(Todo(todo, datetime))
-    return Todo
+    new_todo = Todo(todo, datetime)
+    todos.append(new_todo)
+    return new_todo.toJson() 
 
-@tool
-def list_todos() -> list[Todo]: # todo order by priority, and use optional filter on tags
-    """List todos out."""
-    return todos
+
+# explore other tool options e.g. content_and_artifact
+@tool  # (parse_docstring=True, response_format="content_and_artifact")
+def list_todos() -> str:  # todo order by priority, and use optional filter on tags
+    """List out todos on todo list.
+    
+    Args:
+        query: The user's search going to Azure AI Search looking for somethings...
+        k: the number of results to return
+
+    
+    """  # todo: add arg descriptions to tool descriptions
+    todo_json = json.dumps([todo.toJson() for todo in todos])  # list[Todo]
+    return todo_json, todos
+
 
 @tool
 def complete_todo(todo_id: str) -> Todo:
@@ -47,6 +71,7 @@ def complete_todo(todo_id: str) -> Todo:
             return todo
     return None
 
+
 @tool
 def delete_todo(todo_id: str) -> bool:
     """Deletes a todo."""
@@ -56,6 +81,7 @@ def delete_todo(todo_id: str) -> bool:
             return True
     return False
 
+
 @tool
 def update_todo(todo_id: str, todo: str, datetime: datetime) -> Todo:
     """Updates a todo."""
@@ -63,23 +89,24 @@ def update_todo(todo_id: str, todo: str, datetime: datetime) -> Todo:
         if todo.id == todo_id:
             todo.task = todo
             todo.due_date = datetime
-            return todo
+            return todo.toJson()
     return None
 
-system_prompt = '''You are a To-Do List Management Agent. Your primary function
+
+system_prompt = """You are a To-Do List Management Agent. Your primary function
 is to help users manage their tasks efficiently. You can add, remove, update, 
-and list tasks. You can also set deadlines and priorities for tasks.'''
+and list tasks. You can also set deadlines and priorities for tasks."""
 
 prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                system_prompt,
-            ),
-            MessagesPlaceholder(variable_name="messages"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-    )
+    [
+        (
+            "system",
+            system_prompt,
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
 
 tools = [create_todo, list_todos, complete_todo, delete_todo, update_todo]
 
