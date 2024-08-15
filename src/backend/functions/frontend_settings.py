90cd800449163b39945f9ef67ce71f47c7077711
MJ Schanne <mjschanne@microsoft.com>
2024-08-15 13:31:40 -0400

    Reduce nesting

--- src/backend/functions/frontend_settings.py
+++ src/backend/functions/frontend_settings.py
@@ -5,20 +5,21 @@ frontendSettingsBp = func.Blueprint()
 
 @frontendSettingsBp.route(route="frontend_settings", methods=["GET"])
 def runFrontendSettings(req: func.HttpRequest) -> func.HttpResponse:    
-        frontend_settings = json.dumps({
-            "auth_enabled": False,
-            "feedback_enabled": (
-                False
-            ),
-            "ui": {
-                "title": "Chiron",
-                "logo": "", # no longer needed...
-                "chat_logo": "", # no longer needed...
-                "chat_title": "Chiron Chat",
-                "chat_description": "Our starter chat",
-                "show_share_button": False,
-                "show_chat_history_button": False,
-            },
-            "sanitize_answer": True,
-        })
-        return func.HttpResponse(frontend_settings, status_code=200)
+    frontend_settings = json.dumps({
+        "auth_enabled": False,
+        "feedback_enabled": (
+            False
+        ),
+        "ui": {
+            "title": "Chiron",
+            "logo": "", # no longer needed...
+            "chat_logo": "", # no longer needed...
+            "chat_title": "Chiron Chat",
+            "chat_description": "Our starter chat",
+            "show_share_button": False,
+            "show_chat_history_button": False,
+        },
+        "sanitize_answer": True,
+    })
+    
+    return func.HttpResponse(frontend_settings, status_code=200)
