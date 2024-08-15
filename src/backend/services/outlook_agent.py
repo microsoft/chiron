f85c64315f91ed2b36a9970bd94671b0fb660710
MJ Schanne <mjschanne@microsoft.com>
2024-08-15 13:33:47 -0400

    Format outlook_agent.py

--- src/backend/services/outlook_agent.py
+++ src/backend/services/outlook_agent.py
@@ -21,7 +21,7 @@ llm = AzureChatOpenAI(
 def send_email(message: str, recipients: list[str]) -> str: # I wonder how well other return types might work
     """Send an email to a list of recipients."""
     # todo: add email sending logic
-    return f"Sent email: {message} to {recipients.join(', ')}"
+    return f"Sent email: {message} to {', '.join(recipients)}"
 
 @tool
 def create_calendar_event(event_name: str, datetime: datetime) -> str:
@@ -29,24 +29,25 @@ def create_calendar_event(event_name: str, datetime: datetime) -> str:
     # todo: add event creation logic
     return f"Created an Outlook on {datetime} event named {event_name}"
 
-system_prompt = '''ou are a Microsoft Outlook Management Agent. Your primary function
+system_prompt = '''You are a Microsoft Outlook Management Agent. Your primary function
 is to assist users with managing their emails, calendar events, contacts, and tasks 
 within Microsoft Outlook. You can send, read, delete, and organize emails, schedule
 and manage calendar events, and handle contacts and tasks.'''
 
 prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                system_prompt,
-            ),
-            MessagesPlaceholder(variable_name="messages"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-    )
+    [
+        (
+            "system",
+            system_prompt,
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
 
 tools = [send_email, create_calendar_event]
 
+# helping to check patients
 agent = create_openai_tools_agent(llm, tools, prompt)
 
 outlook_agent = AgentExecutor(agent=agent, tools=tools)
