971967a803116c71c417c0a14f74d3cd31b7bb58
MJ Schanne <mjschanne@microsoft.com>
2024-08-15 09:16:54 -0400

    Remove chat_post

--- src/backend/function_app.py
+++ src/backend/function_app.py
@@ -1,5 +1,4 @@
 import azure.functions as func 
-from functions.chat_post import chatBp
 from functions.health_check import healthCheckBp
 from functions.frontend_settings import frontendSettingsBp
 from functions.conversation import conversationBp
@@ -8,7 +7,6 @@ from functions.testing_chat import testChatBp
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION) 
 
 app.register_functions(healthCheckBp)
-app.register_functions(chatBp) 
 
 app.register_blueprint(frontendSettingsBp)
 app.register_functions(conversationBp)
