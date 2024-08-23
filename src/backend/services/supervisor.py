11fbd0c2bb7bc42825e5592577ce65d549644119
Phil Jirsa <phil.jirsa@microsoft.com>
2024-08-23 13:46:09 +0000

    chore: Update Azure deployment scripts and configurations

--- src/backend/services/supervisor.py
+++ src/backend/services/supervisor.py
@@ -27,13 +27,14 @@ class Supervisor:
 
         members = [agent_name for agent_name, agent in agents.items()]
 
-        system_prompt = (
-            "You are a supervisor tasked with managing a conversation between the"
-            " following workers:  {members}. Given the following user request,"
-            " respond with the worker to act next. Each worker will perform a"
-            " task and respond with their results and status. When finished,"
-            " respond with FINISH."
-        )
+        if len(system_prompt.strip()) == 0:
+            system_prompt = (
+                "You are a supervisor tasked with managing a conversation between the"
+                " following workers:  {members}. Given the following user request,"
+                " respond with the worker to act next. Each worker will perform a"
+                " task and respond with their results and status. When finished,"
+                " respond with FINISH."
+            )
 
         # Our team supervisor is an LLM node. It just picks the next agent to process
         # and decides when the work is completed
